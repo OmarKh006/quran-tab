855c97dd803ea253b369ca4872fd1dc980c27364
OmarKh006 <khairyomar006@gmail.com>
2026-04-15 06:32:18 +0200

    taking html/css from the start code , adding animation feature

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 import * as utils from "./utils.js";
 
 const labels = document.querySelectorAll("label");
+const sectionElements = document.querySelectorAll("section");
 
 labels.forEach(utils.labalClicked);
+
+utils.observeElements(sectionElements);
--- utils.js
+++ utils.js
@@ -3,3 +3,23 @@ export const labalClicked = (item) => {
     event.key === "Enter" && item.click();
   });
 };
+
+export const observeElements = (sectionElements) => {
+  const options = { threshold: 1 };
+
+  const callback = (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("in-view");
+      } else {
+        entry.target.classList.remove("in-view");
+      }
+    });
+  };
+
+  const observer = new IntersectionObserver(callback, options);
+
+  sectionElements.forEach((element) => {
+    observer.observe(element);
+  });
+};
